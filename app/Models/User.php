1a9b74425ea0e4ce81f536a31c03e86659b63ea7
Akmal Khan <khan.akmal542@gmail.com>
2020-12-14 22:26:42 +0500

    get uuid lesson 2

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,10 +6,21 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Str;
 
 class User extends Authenticatable
 {
     use HasFactory, Notifiable;
+
+    public $incrementing = false;
+
+    protected static function boot()
+    {
+        parent::boot();
+        static::creating(function ($model){
+            $model->{$model->getKeyName()} = (string) Str::uuid();
+        });
+    }
 
     /**
      * The attributes that are mass assignable.
